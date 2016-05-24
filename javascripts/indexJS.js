5ad75568d97bb95b5483a96b1bdb1fdbbd8ad4c7
CG <1542723437@qq.com>
2016-05-24 13:58:19 +0800

    update for introduce

--- javascripts/indexJS.js
+++ javascripts/indexJS.js
@@ -8,7 +8,7 @@ function setSize() {
     var maxWidth = window.innerWidth,maxHeight = window.innerHeight;
     document.getElementById('d_content').style.height = maxHeight + 130 + 'px';
     document.getElementById('d_content').style.marginTop = (5*maxHeight)/36 + 'px';
-    document.getElementById('d_content_middle').style.height = (0.48*maxHeight) + 'px';
+    document.getElementById('d_content_middle').style.height = (0.5*maxHeight) + 'px';
     document.getElementById('div_p_introduce').style.marginLeft = ((0.8*maxWidth) - 500)/2 + 'px';
     document.getElementById('bt_into').style.marginLeft = (0.8*maxWidth - 0.15*maxWidth)/2 + 'px';
 
